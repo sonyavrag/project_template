865f1a8971ebcf9f46d08b41869bee8e6923153b
Sofiia Panasiuk <soffipanna@gmail.com>
2025-04-06 19:28:49 +0300

    Create functions for file input and output

---
+++ app/io/input.py
@@ -0,0 +1,8 @@
+def input_text():
+    pass
+
+def read_file_python(file_path):
+    pass
+
+def read_file_pandas(file_path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,5 @@
+def print_text(text):
+    pass
+
+def write_file_python(file_path, content):
+    pass
